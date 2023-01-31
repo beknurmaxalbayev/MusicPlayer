67eeb10678604a0492915ff110a001883f5c3dd3
Beknur Maxalbayev <120553658+beknurmaxalbayev@users.noreply.github.com>
2023-01-31 15:41:29 +0500

    music player

---
+++ js/music-list.js
@@ -0,0 +1,63 @@
+
+let allMusic = [
+  {
+    name: "Бозторғай Ана",
+    artist: "Бекнур Махалбаев",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Там тау адам",
+    artist: "Machenski",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Карман",
+    artist: "Tural Everest и Руслан Добрый",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name:"Чёрные глаза",
+    artist: "Айдамир Мугу",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Кундер отуде",
+    artist: "Айкын Толепберген",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Ұнап қалдың анама",
+    artist: "Ернар Айдар",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Анашым аман жүрші",
+    artist: "Жолдасбек Абдиханов",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Унсиз журек",
+    artist: "Кайрат Нуртас",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Оз журегиннен уялмайсынба",
+    artist: "Кенес Алимжан",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Алаканымнан ушкан аккуым",
+    artist: "Nurbullin, Торегали, Куандык, Ернар, Кайрат, Еркiн",
+    img: "music-10",
+    src: "music-10"
+  },
+];
